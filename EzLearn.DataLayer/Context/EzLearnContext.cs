3a0c4e8b22b695865eff8f18975dcebd64909c53
imansafari33 <Iman.safari33@gmail.com>
2020-05-24 13:38:44 +0430

    InitialDB

---
+++ EzLearn.DataLayer/Context/EzLearnContext.cs
@@ -0,0 +1,26 @@
+﻿using EzLearn.DataLayer.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzLearn.DataLayer.Context
+{
+    public class EzLearnContext:DbContext
+    {
+
+        public EzLearnContext(DbContextOptions<EzLearnContext> options) : base(options)
+        {
+
+        }
+
+        #region User
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+
+        #endregion
+    }
+}
